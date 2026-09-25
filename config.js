bec1bd1cb7089ffb9b40128d70fb102e4d6c419c
Siddhant2037 <siddhanttak2037@gmail.com>
2026-09-25 21:26:58 +0530

    add

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // TRACE-X prototype website configuration
 const PROTOTYPE_URL = "https://siddhant2037.github.io/SIH-2026/";
-const YOUTUBE_VIDEO_ID = "YOUR_VIDEO_ID";
+const YOUTUBE_VIDEO_ID = "K2AwqlFO3pc";
